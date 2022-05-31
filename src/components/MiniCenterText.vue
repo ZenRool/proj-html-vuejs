31270dcd75b9891e8de16a31ab9d73925c0b84e5
Zenrool <lorenzopapperini@hotmail.it>
2022-05-31 10:33:31 +0200

    Modello scritta centrale ripetuta più piccola

---
+++ src/components/MiniCenterText.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    class="center-text container container-small d-flex align-items-center flex-column"
+  >
+    <img :src="require(`../assets/images/icon-${elem.icon}.png`)" alt="icon" />
+    <h3>{{ elem.title }}</h3>
+    <p>{{ elem.paragraph }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "MiniCenterText",
+  props: {
+    elem: {
+      icon: Number,
+      title: String,
+      paragraph: String,
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+@import "../style/variables.scss";
+* {
+  margin: 1.2rem 0;
+  text-align: center;
+}
+div {
+  margin-top: 2.5rem;
+  padding: 0 0.5rem;
+  p {
+    line-height: 2rem;
+    font-size: 0.9rem;
+  }
+  img {
+    width: 80px;
+    height: 80px;
+  }
+}
+</style>
--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -1,14 +1,25 @@
 <template>
   <main id="departments">
-    <CenterText :elem="this.bigElement[0]" />
+    <section>
+      <CenterText :elem="this.bigElement[0]" />
+      <div class="container container-regular d-flex">
+        <MiniCenterText
+          v-for="elem in this.welcome"
+          :key="elem.icon"
+          :elem="elem"
+        />
+      </div>
+    </section>
   </main>
 </template>
 <script>
 import CenterText from "./CenterText.vue";
+import MiniCenterText from "./MiniCenterText.vue";
 export default {
   name: "MyMain",
   components: {
     CenterText,
+    MiniCenterText,
   },
   data: function () {
     return {
@@ -19,6 +30,32 @@ export default {
           line: false,
           paragraph:
             "Sed ut perspiciatis unde omnis iste naus error sit voluptatem accusatium doloremque ladantium, totam rem aperiam, eaque ipsa quae ab illo",
+        },
+      ],
+      welcome: [
+        {
+          icon: 1,
+          title: "Same day appointments",
+          paragraph:
+            "Sed ut perspiciatis unde omnis iste naus error sit voluptatem accusatium",
+        },
+        {
+          icon: 2,
+          title: "World class facilities",
+          paragraph:
+            "Sed ut perspiciatis unde omnis iste naus error sit voluptatem accusatium",
+        },
+        {
+          icon: 3,
+          title: "Expert doctors",
+          paragraph:
+            "Sed ut perspiciatis unde omnis iste naus error sit voluptatem accusatium",
+        },
+        {
+          icon: 4,
+          title: "Complementary therapies",
+          paragraph:
+            "Sed ut perspiciatis unde omnis iste naus error sit voluptatem accusatium",
         },
       ],
     };
